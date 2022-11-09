163269c9f67393fe22f666262405f83d0fc1cc29
gyk <3490315473@qq.com>
2022-11-09 15:16:11 +0800

    commit-006-filter02

--- 006-springboot-filter/src/main/java/cn/edu/hncj/config/WebApplicationConfig.java
+++ 006-springboot-filter/src/main/java/cn/edu/hncj/config/WebApplicationConfig.java
@@ -1,13 +1,23 @@
 package cn.edu.hncj.config;
 
 import cn.edu.hncj.web.MyFilter;
+import cn.edu.hncj.web.MyServlet;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
 @Configuration
 public class WebApplicationConfig {
+
+    //定义方法，注册Servlet对象
+    @Bean
+    public ServletRegistrationBean servletRegistrationBean(){
+        ServletRegistrationBean bean =
+                new ServletRegistrationBean(new MyServlet(),"/myServlet");
+        return bean;
+    }
 
     //注册字符集过滤器
     @Bean
